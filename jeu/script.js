d0ebf95b0e39555ee2d82962beafaa063bfef642
Diego Ply <ply.dgo@gmail.com>
2023-10-03 18:34:47 +0200

    debut-compteur

--- jeu/script.js
+++ jeu/script.js
@@ -12,6 +12,8 @@ function choixNumDés(){
         $(document).ready(function(){
             $('#imageDés').html(`<img src="./image/Dés 1.png">`);
         });
+        var valeurDés = 1;
+        return valeurDés;
 
     }
 
@@ -20,36 +22,54 @@ function choixNumDés(){
         $(document).ready(function(){
             $('#imageDés').html(`<img src="./image/Dés 2.png">`);
         });
+
+        var valeurDés = 2;
+        return valeurDés;
     }
     else if (numDés == 3){
 
         $(document).ready(function(){
             $('#imageDés').html(`<img src="./image/Dés 3.png">`);
         });
+
+        var valeurDés = 3;
+        return valeurDés;
     }
     else if (numDés == 4){
 
         $(document).ready(function(){
             $('#imageDés').html(`<img src="./image/Dés 4.png">`);
         });
+
+        var valeurDés = 4;
+        return valeurDés;
     }
     else if (numDés == 5){
         
         $(document).ready(function(){
             $('#imageDés').html(`<img src="./image/Dés 5.png">`);
         });
+
+        var valeurDés = 5;
+        return valeurDés;
     }
     else{
        
         $(document).ready(function(){
             $('#imageDés').html(`<img src="./image/Dés 6.png">`);
         });
+         var valeurDés = 6;
+        return valeurDés;
     }
 }
 
 
+
 $('#lancerDés').on('click',function(){
-    choixNumDés()
+    choixNumDés();
+   
+
 });
+
 
 
