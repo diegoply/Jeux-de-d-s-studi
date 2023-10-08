93220ca7fd32269f0e87fd316a3601111b9134b3
Diego Ply <ply.dgo@gmail.com>
2023-10-08 10:57:21 +0200

    debut 2 joueur

--- jeu/script.js
+++ jeu/script.js
@@ -3,14 +3,28 @@ $('#buttonStart').on('click', function () {
     let comptHold = 0;
     comptDec = 0;
 
+    let compt2 = 0;
+    let comptHold2 = 0;
+    comptDec2 = 0;
+
 
     $('#scorePlayer1').html(0);
     $('#currentPlayer1').html(0);
+    $('#scorePlayer2').html(0);
+    $('#currentPlayer2').html(0);
 
-    let joueur = true;
+    var joueur = true;
+    choixJoueur();
 
-    player1();
+    function choixJoueur(){
 
+    if(joueur === true){
+        player1();
+    }
+    else{
+        player2();
+    }
+    }
     function player1() {
 
 
@@ -91,7 +105,7 @@ $('#buttonStart').on('click', function () {
                 compt = compt + numDes;
 
 
-                $('#scorePlayer1').html(compt);
+                $('#scorePlayer2').html(compt);
                 console.log("compteur " + compt);
                 return compt;
 
@@ -104,11 +118,12 @@ $('#buttonStart').on('click', function () {
             comptHold = comptHold + compt;
 
             console.log("compteurHold " + comptHold);
-            $('#currentPlayer1').html(comptHold);
+            $('#currentPlayer2').html(comptHold);
             compt = 0;
             return comptHold;
         });
 
+    
 
 
 
@@ -130,4 +145,130 @@ $('#buttonStart').on('click', function () {
         }
 
     };
+});
+
+
+//debut player2//
+
+
+function player2() {
+
+
+    $('#lancerDés').on('click', function () {
+
+        let numDes2 = Math.floor(Math.random() * 6) + 1;
+
+        choixNumDés2(numDes2);
+        compt2 = compteur2(compt2, numDes2);
+        comptDecompte2(compt2, comptHold2);
+
+
+    });
+    function choixNumDés2(numDes2) {
+
+
+
+
+        if (numDes2 == 1) {
+
+
+            $('#imageDés').html(`<img src="./image/Dés 1.png" >`);
+
+            console.log("num dé " + numDes2);
+        }
+
+        else if (numDes2 == 2) {
+
+
+            $('#imageDés').html(`<img src="./image/Dés 2.png">`);
+            console.log("num dé " + numDes2);
+        }
+        else if (numDes2 == 3) {
+
+
+            $('#imageDés').html(`<img src="./image/Dés 3.png">`);
+            console.log("num dé " + numDes2);
+
+        }
+        else if (numDes2 == 4) {
+
+
+            $('#imageDés').html(`<img src="./image/Dés 4.png">`);
+            console.log("num dé " + numDes2);
+        }
+        else if (numDes2 == 5) {
+
+
+            $('#imageDés').html(`<img src="./image/Dés 5.png">`);
+            console.log("num dé " + numDes2);
+        }
+        else {
+
+
+            $('#imageDés').html(`<img src="./image/Dés 6.png">`);
+            console.log("num dé " + numDes2);
+        }
+
+
+    };
+
+
+
+    function compteur2(compt2, numDes2) {
+
+        if (numDes2 === 1) {
+            compt2 = 0;
+
+            console.log(compt2);
+            return compt2;
+        }
+
+        else {
+
+            console.log("compt" + compt2);
+            console.log("num" + numDes2);
+
+            compt2 = compt2 + numDes2;
+
+
+            $('#scorePlayer1').html(compt2);
+            console.log("compteur " + compt2);
+            return compt2;
+
+        };
+    };
+
+
+    $('#buttonHold').on('click', function () {
+
+        comptHold2 = comptHold2 + compt2;
+
+        console.log("compteurHold " + comptHold2);
+        $('#currentPlayer1').html(comptHold2);
+        compt2 = 0;
+        return comptHold2;
+    });
+
+
+
+
+
+
+    let comptDec2 = 0;
+
+
+    function comptDecompte2(compt2, comptHold2) {
+        comptDec2 = comptHold2 + compt2;
+        console.log("comptDecompte " + comptDec2);
+
+        if (comptDec2 >= 100) {
+            $('#winner').html('Joueur 1 Gagne');
+            console.log("victoire");
+        }
+        else {
+            $('#winner').html('Jeux en cours');
+        }
+    }
+
+};
 });
